cef66ebfdcaadb582d355382d61acfbd344efc7b
Vanherwegentim <Tim.vanherwegen@student.ucll.be>
2022-03-10 15:29:59 +0100

    Add a way to place a carorder

--- src/main/java/be/kuleuven/assemassit/Controller/OrderController.java
+++ src/main/java/be/kuleuven/assemassit/Controller/OrderController.java
@@ -1,16 +1,25 @@
 package be.kuleuven.assemassit.Controller;
 
+import be.kuleuven.assemassit.Domain.CarModel;
 import be.kuleuven.assemassit.Domain.CarOrder;
 import be.kuleuven.assemassit.Domain.GarageHolder;
 
 import java.util.List;
 
 public class OrderController {
+  //assemblyLine moet nog meegegeven worden in de constructor omdat we carModels nodig hebben om weer te geven in de ui
+  public OrderController(){}
+  private List<GarageHolder> garageHolders;
+  private GarageHolder loggedInGarageHolder;
 
-    private List<GarageHolder> garageHolders;
+  public List<CarOrder> giveNewCarOrders(GarageHolder garageHolder){
+    return List.copyOf(garageHolder.getCarOrders());
+  }
 
-    public List<CarOrder> giveNewCarOrders(GarageHolder garageHolder){
-        return List.copyOf(garageHolder.getCarOrders());
-        //yeet
-    }
+  //should carModel be String or CarModel object?
+
+  public void placeCarOrder(CarModel carModel, String body, String color, String engine, String gearbox, String seats, String airco, String wheels){
+    // ik heb hier geen logica gezet die het car object aanmaakt met deze variabelen door low-coupling, klopt dit?
+    loggedInGarageHolder.addCarOrder(carModel, body, color, engine, gearbox, seats, airco, wheels);
+  }
 }
--- src/main/java/be/kuleuven/assemassit/Domain/GarageHolder.java
+++ src/main/java/be/kuleuven/assemassit/Domain/GarageHolder.java
@@ -1,12 +1,27 @@
 package be.kuleuven.assemassit.Domain;
 
+import be.kuleuven.assemassit.Domain.Enums.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class GarageHolder {
-    private List<CarOrder> carOrders = new ArrayList<>();
-    //TODO misschien een set gebruiken hier? is orde en duplicates belangrijk?
-    public List<CarOrder> getCarOrders(){
-        return List.copyOf(carOrders);
-    }
+  private List<CarOrder> carOrders = new ArrayList<>();
+  public List<CarOrder> getCarOrders(){
+    return List.copyOf(carOrders);
+  }
+
+  public void addCarOrder(CarModel carModel,String body, String color, String engine, String gearbox, String seats, String airco, String wheels){
+    Body bodyEnum = Body.valueOf(body);
+    Color colorEnum = Color.valueOf(color);
+    Engine engineEnum = Engine.valueOf(engine);
+    Gearbox gearboxEnum = Gearbox.valueOf(gearbox);
+    Seat seatEnum = Seat.valueOf(seats);
+    Airco aircoEnum = Airco.valueOf(airco);
+    Wheel wheelsEnum = Wheel.valueOf(wheels);
+
+    Car car = new Car(carModel, bodyEnum, colorEnum, engineEnum,gearboxEnum,seatEnum,aircoEnum,wheelsEnum );
+    CarOrder carOrder = new CarOrder(car);
+    carOrders.add(carOrder);
+  }
 }
