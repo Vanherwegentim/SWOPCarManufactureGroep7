86333ace428438d79dcd03c8da87231ade464ffb
Robbe Ramon <robbe.ramon@student.hogent.be>
2022-03-10 21:11:14 +0100

    Refactor WorkPosts

--- src/main/java/be/kuleuven/assemassit/Domain/WorkPost.java
+++ src/main/java/be/kuleuven/assemassit/Domain/WorkPost.java
@@ -1,18 +1,33 @@
 package be.kuleuven.assemassit.Domain;
 
+import be.kuleuven.assemassit.Domain.AssemblyTask;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public interface WorkPost {
-  List<AssemblyTask> assemblyTasks = new ArrayList<AssemblyTask>();
+public class WorkPost {
+  private int id;
+  private List<AssemblyTask> assemblyTasks;
 
-	public int getId();
+  public WorkPost() {
+    this.assemblyTasks = new ArrayList<AssemblyTask>();
+  }
 
-	public List<AssemblyTask> givePendingAssemblyTasks();
+  public int getId() {
+    return this.id;
+  }
 
-	public void completeAssemblyTask(int assemblyTaskId);
+  public List<AssemblyTask> givePendingAssemblyTasks() {
+    return assemblyTasks.stream()
+      .filter(at -> at.getPending() == true)
+      .collect(Collectors.toList());
+  }
+
+  public void completeAssemblyTask(int assemblyTaskId) {
+    findAssemblyTask(assemblyTaskId).complete();
+  }
 
   private AssemblyTask findAssemblyTask(int id) {
     Optional<AssemblyTask> assemblyTask = assemblyTasks.stream()
