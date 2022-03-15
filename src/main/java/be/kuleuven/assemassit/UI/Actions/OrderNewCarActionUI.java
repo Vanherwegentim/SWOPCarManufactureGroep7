90668519ad9a91ca24fdfbdd57c0d9dd15a05fe0
Ward Beyens <wardbeyens99@gmail.com>
2022-03-15 23:04:40 +0100

    Add Order New Car use case UI

--- src/main/java/be/kuleuven/assemassit/UI/Actions/OrderNewCarActionUI.java
+++ src/main/java/be/kuleuven/assemassit/UI/Actions/OrderNewCarActionUI.java
@@ -3,6 +3,7 @@ package be.kuleuven.assemassit.UI.Actions;
 import be.kuleuven.assemassit.Controller.AssemblyLineController;
 import be.kuleuven.assemassit.Controller.OrderController;
 import be.kuleuven.assemassit.UI.Actions.Overviews.GarageHolderActionsOverviewUI;
+import be.kuleuven.assemassit.UI.MainUI;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -11,7 +12,7 @@ import java.util.*;
 public class OrderNewCarActionUI {
   public static void run(OrderController orderController, AssemblyLineController assemblyLineController) {
     Scanner scanner = new Scanner(System.in);
-    int action;
+    int choice;
 
     do {
       System.out.println("Orders placed by: " + orderController.giveLoggedInGarageHolderName());
@@ -23,71 +24,95 @@ public class OrderNewCarActionUI {
       orderController.giveCompletedCarOrders().forEach(System.out::println);
 
       System.out.println("Please choose an action:");
-      System.out.println("1: Place a new order");
-      System.out.println("0: Go back");
+      System.out.println(" 1: Place a new order");
+      System.out.println("-1: Go back");
 
-      action = scanner.nextInt();
+      choice = scanner.nextInt();
 
-      switch (action) {
+      switch (choice) {
         case 1 -> {
           int chosenCarModelId = displayChooseCarModel(orderController.giveListOfCarModels());
 
-          Map<String, String> selectedParts = displayOrderingForm(orderController.givePossibleOptionsOfCarModel(chosenCarModelId));
+          Map<String, List<String>> parts = orderController.givePossibleOptionsOfCarModel(chosenCarModelId);
+
+          // TODO: replace with private method
+          //Map<String, String> selectedParts = displayOrderingForm(orderController.givePossibleOptionsOfCarModel(chosenCarModelId));
+          Map<String, String> selectedParts = new HashMap<>();
+          for (Map.Entry<String, List<String>> part : parts.entrySet()) {
+            Scanner scanner2 = new Scanner(System.in);
+            int optionIndex;
+            List<String> options = part.getValue();
+            do {
+              System.out.println("Choose the option for: " + part.getKey());
+              for (int i = 0; i < options.size(); i++) {
+                System.out.println(String.format("%2d", i) + ": " + options.get(i));
+              }
+              System.out.println("-1: Cancel placing the order");
+              optionIndex = scanner2.nextInt();
+              if (optionIndex == -1){
+                OrderNewCarActionUI.run(orderController, assemblyLineController);
+                break;
+              }
+            } while (optionIndex < 0 || optionIndex > options.size() - 1);
+            selectedParts.put(part.getKey(), part.getValue().get(optionIndex));
+            System.out.println("Chosen: " + options.get(optionIndex));
+          }
 
           orderController.placeCarOrder(chosenCarModelId, selectedParts.get("Body"), selectedParts.get("Color"), selectedParts.get("Engine"), selectedParts.get("GearBox"), selectedParts.get("Seats"), selectedParts.get("Airco"), selectedParts.get("Wheels"));
-          //int orderId = orderController.placeCarOrder(chosenCarModelId, selectedParts.get("Body"), selectedParts.get("Color"), selectedParts.get("Engine"), selectedParts.get("GearBox"), selectedParts.get("Seats"), selectedParts.get("Airco"), selectedParts.get("Wheels"));
-          // TODO: Hoe krijg ik hier de orderId ???
-
-          // TODO: updates the production schedule
+          //LocalDateTime estimatedCompletionDate = orderController.placeCarOrder(chosenCarModelId, selectedParts.get("Body"), selectedParts.get("Color"), selectedParts.get("Engine"), selectedParts.get("GearBox"), selectedParts.get("Seats"), selectedParts.get("Airco"), selectedParts.get("Wheels"));
+          // TODO: placeCarOrder geeft een localdatetime terug
 
           LocalDateTime estimatedCompletionDate = LocalDateTime.now().plusDays(3);
-          // TODO: de echte estimatedCompletionDate activeren
-          //estimatedCompletionDate = orderController.getCompletionDate(0);
 
           DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
           System.out.println("The estimated completion date for the order is: " + estimatedCompletionDate.format(formatter));
 
-          GarageHolderActionsOverviewUI.run(orderController, assemblyLineController);
+          OrderNewCarActionUI.run(orderController, assemblyLineController);
         }
-        case 0 -> OrderNewCarActionUI.run(orderController, assemblyLineController);
+        case -1 -> GarageHolderActionsOverviewUI.run(orderController, assemblyLineController);
       }
-    } while (action < 0 || action > 1);
+    } while (choice != -1 && (choice != 1));
   }
 
-  public static Integer displayChooseCarModel(List<String> carModels) {
+  private static Integer displayChooseCarModel(Map<Integer, String> carModels) {
     Scanner scanner = new Scanner(System.in);
     int carModelId;
 
     do {
       System.out.println("Please choose the model:");
-      carModels.forEach(System.out::println);
+      carModels.forEach((id, name) -> System.out.println(id + ": " + name));
       carModelId = scanner.nextInt();
-    } while (carModelId < 0 || carModelId > carModels.size() - 1);
+    } while (!carModels.containsKey(carModelId));
 
-    //TODO: is deze regex the way forward?
-    System.out.println("Chosen model: " + carModels.get(carModelId).split(": ")[1]);
+    System.out.println("Chosen model: " + carModels.get(carModelId));
     return carModelId;
   }
 
 
-  public static Map<String, String> displayOrderingForm(Map<String, List<String>> parts) {
+  private static Map<String, String> displayOrderingForm(Map<String, List<String>> parts) {
     Map<String, String> selectedParts = new HashMap<>();
 
-    for (Map.Entry<String, List<String>> entry : parts.entrySet()) {
+    for (Map.Entry<String, List<String>> part : parts.entrySet()) {
       Scanner scanner = new Scanner(System.in);
       int optionIndex;
-      List<String> options = entry.getValue();
+      List<String> options = part.getValue();
 
-      System.out.println("Choose the option for: " + entry.getKey());
+      System.out.println("Choose the option for: " + part.getKey());
 
       do {
         for (int i = 0; i < options.size(); i++) {
-          System.out.println(i + ": " + options.get(i));
+          System.out.println(String.format("%2d", i) + ": " + options.get(i));
         }
+        System.out.println("-1: Cancel placing the order");
+
         optionIndex = scanner.nextInt();
+
+        if (optionIndex == -1){
+          // TODO: stop the code and return to menu
+        }
       } while (optionIndex < 0 || optionIndex > options.size() - 1);
 
-      selectedParts.put(entry.getKey(), entry.getValue().get(optionIndex));
+      selectedParts.put(part.getKey(), part.getValue().get(optionIndex));
       System.out.println("Chosen: " + options.get(optionIndex));
     }
     return selectedParts;
