a9e4bedd356d65b0c800c7c707a70535b0232ebc
Ward Beyens <wardbeyens99@gmail.com>
2022-03-16 15:46:41 +0100

    Update AssemblyLine statusses

--- src/main/java/be/kuleuven/assemassit/Controller/AssemblyLineController.java
+++ src/main/java/be/kuleuven/assemassit/Controller/AssemblyLineController.java
@@ -4,11 +4,11 @@ import be.kuleuven.assemassit.Domain.AssemblyLine;
 import be.kuleuven.assemassit.Domain.AssemblyTask;
 import be.kuleuven.assemassit.Domain.WorkPost;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AssemblyLineController {
 
@@ -23,9 +23,7 @@ public class AssemblyLineController {
   }
 
   public Map<Integer, String> giveAllWorkPosts() {
-    return List
-      .of(assemblyLine.getAccessoriesPost(), assemblyLine.getCarBodyPost(), assemblyLine.getDrivetrainPost())
-      .stream()
+    return Stream.of(assemblyLine.getAccessoriesPost(), assemblyLine.getCarBodyPost(), assemblyLine.getDrivetrainPost())
       .collect(Collectors.toMap(WorkPost::getId, (wp -> wp.getWorkPostType().toString())));
   }
 
@@ -51,20 +49,36 @@ public class AssemblyLineController {
     return assemblyLine.giveCarAssemblyTask(workPostId, assemblyTaskid).getActions();
   }
 
-  public Map<String, List<String>> giveAssemblyLineStatusAndOverview(int statusId) {
+  public Map<String, List<String>> giveAssemblyLineStatusOverview() {
+    //TODO: !!!REFACTOR THIS SHIT!!!
+
+    Map<String, AssemblyTask> assemblyLineStatus = assemblyLine.giveStatus();
+    Map<String, List<AssemblyTask>> workPostPairs = assemblyLine.giveTasksOverview();
+
+    return evaluateAssemblyLineStatusOverview(assemblyLineStatus, workPostPairs);
+  }
+
+  public Map<String, List<String>> giveFutureAssemblyLineStatusOverview() {
+    //TODO: !!!REFACTOR THIS SHIT!!!
+
+    Map<String, AssemblyTask> assemblyLineStatus = assemblyLine.giveStatus();
+    Map<String, List<AssemblyTask>> workPostPairs = assemblyLine.giveFutureTasksOverview();
+
+    return evaluateAssemblyLineStatusOverview(assemblyLineStatus, workPostPairs);
+  }
+
+  private Map<String, List<String>> evaluateAssemblyLineStatusOverview(
+    Map<String, AssemblyTask> assemblyLineStatus, Map<String, List<AssemblyTask>> giveTasksOverview) {
     //TODO: !!!REFACTOR THIS SHIT!!!
 
     Map<String, List<String>> output = new HashMap<>();
-
-    Map<String, AssemblyTask> assemblyLineStatus = assemblyLine.giveStatus();
-    Map<String, List<AssemblyTask>> giveTasksOverview = assemblyLine.giveTasksOverview();
 
     for (String key : giveTasksOverview.keySet()) {
       List<AssemblyTask> values = giveTasksOverview.get(key);
       List<String> valuesString = values.stream().map(AssemblyTask::getName).collect(Collectors.toList());
 
       for (int i = 0; i < valuesString.size(); i++) {
-        if (assemblyLineStatus.get(key) == values.get(i)) { //assemblyLineStatus.containsValue(values.get(i))
+        if (assemblyLineStatus.get(key) == values.get(i)) {
           String newValue = valuesString.get(i) + " (active)";
           valuesString.set(i, newValue);
         }
@@ -82,7 +96,7 @@ public class AssemblyLineController {
   }
 
   public String moveAssemblyLine(int minutes) {
-	  return assemblyLine.move(minutes);
+    return assemblyLine.move(minutes);
   }
 }
 
