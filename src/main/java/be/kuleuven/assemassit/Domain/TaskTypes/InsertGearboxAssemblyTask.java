4729f83779d30e13f43d35a5569a6e5a2c48725c
Ward Beyens <wardbeyens99@gmail.com>
2022-03-17 11:23:19 +0100

    Update usecase 2

--- src/main/java/be/kuleuven/assemassit/Domain/TaskTypes/InsertGearboxAssemblyTask.java
+++ src/main/java/be/kuleuven/assemassit/Domain/TaskTypes/InsertGearboxAssemblyTask.java
@@ -4,13 +4,23 @@ import be.kuleuven.assemassit.Domain.AssemblyTask;
 import be.kuleuven.assemassit.Domain.Enums.AssemblyTaskType;
 import be.kuleuven.assemassit.Domain.Enums.Gearbox;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InsertGearboxAssemblyTask extends AssemblyTask {
   private Gearbox gearbox;
   private AssemblyTaskType assemblyTaskType = AssemblyTaskType.INSERT_GEARBOX;
 
-  public InsertGearboxAssemblyTask(Gearbox gearbox ) {
+  public InsertGearboxAssemblyTask(Gearbox gearbox) {
     super("Insert gearbox");
     this.gearbox = gearbox;
+  }
+
+  @Override
+  public List<String> getActions() {
+    List<String> actions = new ArrayList<>();
+    actions.add("Installing the " + gearbox + " gearbox");
+    return actions;
   }
 
   @Override
@@ -20,8 +30,8 @@ public class InsertGearboxAssemblyTask extends AssemblyTask {
 
   //Pattern matching not working here for some reason
   @Override
-  public boolean equals(Object o){
-    if(o instanceof InsertGearboxAssemblyTask){
+  public boolean equals(Object o) {
+    if (o instanceof InsertGearboxAssemblyTask) {
       return this.gearbox == ((InsertGearboxAssemblyTask) o).gearbox;
     }
     return false;
--- src/main/java/be/kuleuven/assemassit/Domain/TaskTypes/InstallAircoAssemblyTask.java
+++ src/main/java/be/kuleuven/assemassit/Domain/TaskTypes/InstallAircoAssemblyTask.java
@@ -4,6 +4,9 @@ import be.kuleuven.assemassit.Domain.AssemblyTask;
 import be.kuleuven.assemassit.Domain.Enums.Airco;
 import be.kuleuven.assemassit.Domain.Enums.AssemblyTaskType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InstallAircoAssemblyTask extends AssemblyTask {
   private Airco airco;
   private AssemblyTaskType assemblyTaskType = AssemblyTaskType.INSTALL_AIRCO;
@@ -14,14 +17,21 @@ public class InstallAircoAssemblyTask extends AssemblyTask {
   }
 
   @Override
+  public List<String> getActions() {
+    List<String> actions = new ArrayList<>();
+    actions.add("Installing the " + airco + " airco");
+    return actions;
+  }
+
+  @Override
   public AssemblyTaskType getAssemblyTaskType() {
     return this.assemblyTaskType;
   }
 
   //Pattern matching not working here for some reason
   @Override
-  public boolean equals(Object o){
-    if(o instanceof InstallAircoAssemblyTask){
+  public boolean equals(Object o) {
+    if (o instanceof InstallAircoAssemblyTask) {
       return this.airco == ((InstallAircoAssemblyTask) o).airco;
     }
     return false;
