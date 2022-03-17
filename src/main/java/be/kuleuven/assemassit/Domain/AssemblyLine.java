732824252af64b811d36776a8f6097c9181d8e5b
Robbe Ramon <robbe.ramon@student.hogent.be>
2022-03-17 20:26:54 +0100

    Attempt to fix the estimated time bug

--- src/main/java/be/kuleuven/assemassit/Domain/AssemblyLine.java
+++ src/main/java/be/kuleuven/assemassit/Domain/AssemblyLine.java
@@ -206,12 +206,17 @@ public class AssemblyLine {
 
   public LocalDateTime giveEstimatedCompletionDateOfLatestProcess() {
     // calculate remaining cars for this day (1)
+    double remaningCarsForTodayDouble = ((double) ((endTime.getHour() * 60 + endTime.getMinute()) - // end time
+      giveManufacturingDurationInMinutes() - // time needed to manufacture a car
+      (LocalTime.now().getHour() * 60 + LocalTime.now().getMinute()) - // current time
+      maxTimeNeededForWorkPostOnLine() + // time needed for the slowest work post
+      60) / (double) 60);
     int remainingCarsForToday =
-      (int) ((double) ((endTime.getHour() * 60 + endTime.getMinute()) - // end time
+      (int) Math.ceil(((double) ((endTime.getHour() * 60 + endTime.getMinute()) - // end time
         giveManufacturingDurationInMinutes() - // time needed to manufacture a car
         (LocalTime.now().getHour() * 60 + LocalTime.now().getMinute()) - // current time
         maxTimeNeededForWorkPostOnLine() + // time needed for the slowest work post
-        60) / (double) 60);
+        60) / (double) 60));
 
     // calculate cars for a whole day (2)
     int amountOfCarsWholeDay =
@@ -234,12 +239,12 @@ public class AssemblyLine {
 
     // car can not be manufactured today
     // Math.ceil(list - (1) / (2)) = days needed
-    int daysNeeded = (carAssemblyProcessesQueue.size() - remainingCarsForToday) / amountOfCarsWholeDay;
+    int daysNeeded = Math.max(0,(carAssemblyProcessesQueue.size() - remainingCarsForToday) / amountOfCarsWholeDay - 1);
 
 
     // return date of tomorrow + days needed + minutes needed
     LocalDateTime today = LocalDateTime.now();
-    int remainingMinutesForLastDay = (((carAssemblyProcessesQueue.size() - remainingCarsForToday) % amountOfCarsWholeDay) + 1) * maxTimeNeededForWorkPostOnLine();
+    int remainingMinutesForLastDay = (((carAssemblyProcessesQueue.size() - Math.abs(remainingCarsForToday)) % amountOfCarsWholeDay) + 1) * maxTimeNeededForWorkPostOnLine();
     return LocalDateTime.of(today.getYear(), today.getMonth(), today.getDayOfMonth(), startTime.getHour(), startTime.getMinute()).plusDays(1).plusDays(daysNeeded).plusMinutes(giveManufacturingDurationInMinutes() - maxTimeNeededForWorkPostOnLine()).plusMinutes(remainingMinutesForLastDay);
   }
 
