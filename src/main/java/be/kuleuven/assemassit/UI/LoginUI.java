ac2dceb7f5f52c328d5f55ea4226803fa4677eca
Robbe Ramon <robbe.ramon@student.hogent.be>
2022-04-03 17:05:00 +0200

    Add login ui class

---
+++ src/main/java/be/kuleuven/assemassit/UI/LoginUI.java
@@ -0,0 +1,12 @@
+package be.kuleuven.assemassit.UI;
+
+import be.kuleuven.assemassit.Controller.AssemblyLineController;
+import be.kuleuven.assemassit.Controller.ControllerFactory;
+import be.kuleuven.assemassit.Controller.OrderController;
+
+public class LoginUI {
+  private ControllerFactory controllerFactory;
+  private OrderController orderController;
+  private AssemblyLineController assemblyLineController;
+
+}
