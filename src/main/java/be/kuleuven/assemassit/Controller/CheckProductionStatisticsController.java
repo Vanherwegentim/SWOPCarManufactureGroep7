5a080fb944084784c2a889f98d7fba8d5d4a46e8
Vanherwegentim <Tim.vanherwegen@student.ucll.be>
2022-04-06 14:20:44 +0200

    update the checkStatistics method in the Controller

--- src/main/java/be/kuleuven/assemassit/Controller/CheckProductionStatisticsController.java
+++ src/main/java/be/kuleuven/assemassit/Controller/CheckProductionStatisticsController.java
@@ -1,10 +1,20 @@
 package be.kuleuven.assemassit.Controller;
 
+import be.kuleuven.assemassit.Domain.AssemblyLine;
+
 public class CheckProductionStatisticsController {
+  private AssemblyLine assemblyLine;
+
+  public CheckProductionStatisticsController(AssemblyLine assemblyLine) {
+    this.assemblyLine = assemblyLine;
+  }
 
 
   public String checkStatistics() {
     //TODO
-    return "";
+    return "The average amount cars made in a day are: " + assemblyLine.averageCarsInADay() + "\n" +
+      "The median amount of cars produced in a day are: " + assemblyLine.medianCarsInADay() + "\n" +
+      "The exact amount of cars made in the last 2 days are: " + assemblyLine.exactCarsIn2Days() + "\n";
+
   }
 }
