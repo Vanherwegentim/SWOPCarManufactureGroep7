ae0252418fa0109267b3ebdfcdfa399ff5c1167d
Vanherwegentim <tim.vanherwegen@student.kuleuven.be>
2022-04-23 18:21:58 +0200

    wrote a part of CheckAssemblyLineStatusActionUI

---
+++ src/main/java/be/kuleuven/assemassit/UI/Actions/CheckAssemblyLineStatusActionUI.java
@@ -0,0 +1,18 @@
+package be.kuleuven.assemassit.UI.Actions;
+
+import be.kuleuven.assemassit.Controller.CheckAssemblyLineStatusController;
+import be.kuleuven.assemassit.UI.UI;
+
+public class CheckAssemblyLineStatusActionUI implements UI {
+
+  public CheckAssemblyLineStatusController checkAssemblyLineStatusController;
+
+  public CheckAssemblyLineStatusActionUI(CheckAssemblyLineStatusController checkAssemblyLineStatusController) {
+    this.checkAssemblyLineStatusController = checkAssemblyLineStatusController;
+  }
+
+  @Override
+  public void run() {
+    while
+  }
+}
--- src/main/java/be/kuleuven/assemassit/UI/Actions/OrderNewCarActionUI.java
+++ src/main/java/be/kuleuven/assemassit/UI/Actions/OrderNewCarActionUI.java
@@ -1,138 +1,39 @@
-package be.kuleuven.assemassit.UI.Actions;
+public void run() {
+    while (true) {
 
-import be.kuleuven.assemassit.Controller.AssemblyLineController;
-import be.kuleuven.assemassit.Controller.OrderNewCarController;
-import be.kuleuven.assemassit.UI.Actions.GarageHolderActions.GarageHolderActionsOverviewUI;
-import be.kuleuven.assemassit.UI.UI;
+      Map<Integer, String> allWorkPosts = performAssemblyTasksController.giveAllWorkPosts();
+      Optional<Integer> chosenWorkPostIdOptional = displayChooseWorkPost(allWorkPosts);
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
+      if (chosenWorkPostIdOptional.isEmpty()) continue; // TODO: show feedback
 
+      int chosenWorkPostId = chosenWorkPostIdOptional.get();
 
-public class OrderNewCarActionUI implements UI {
-  private OrderNewCarController orderNewCarController;
-  private GarageHolderActionsOverviewUI garageHolderActionsOverviewUI;
+      Map<Integer, String> allAssemblyTasks = performAssemblyTasksController.givePendingAssemblyTasks(chosenWorkPostId);
 
-  public OrderNewCarActionUI(OrderNewCarController orderNewCarController) {
-    this.orderNewCarController = orderNewCarController;
-    this.garageHolderActionsOverviewUI = new GarageHolderActionsOverviewUI(this.orderNewCarController);
-  }
-
-  public void run() {
-    Scanner scanner = new Scanner(System.in);
-    int choice;
-
-    do {
-      System.out.println();
-      System.out.println("Orders placed by: " + orderNewCarController.giveLoggedInGarageHolderName());
-
-      System.out.println("Pending:");
-      displayCarOrders(orderNewCarController.givePendingCarOrders());
-
-      System.out.println("History:");
-      displayCarOrders(orderNewCarController.giveCompletedCarOrders());
-
-      System.out.println();
-      System.out.println("Please choose an action:");
-      System.out.println(" 1: Place a new order");
-      System.out.println("-1: Go back");
-
-      choice = scanner.nextInt();
-
-      switch (choice) {
-        case 1 -> {
-          Optional<Integer> chosenCarModelIdOptional = displayChooseCarModel(orderNewCarController.giveListOfCarModels());
-
-          if (chosenCarModelIdOptional.isEmpty()) {
-            run(); //TODO check if dit is de manier juiste redirect naar zichzelf
-            return;
-          }
-
-          int chosenCarModelId = chosenCarModelIdOptional.get();
-
-          Map<String, List<String>> parts = orderNewCarController.givePossibleOptionsOfCarModel(chosenCarModelId);
-
-          Optional<Map<String, String>> selectedPartsOptional = displayOrderingForm(parts);
-
-          if (selectedPartsOptional.isEmpty()) {
-            run(); //TODO check if dit is de manier juiste redirect naar zichzelf
-            return;
-          }
-          Map<String, String> selectedParts = selectedPartsOptional.get();
-
-          LocalDateTime estimatedCompletionDate = orderNewCarController.placeCarOrder(chosenCarModelId, selectedParts.get("Body"), selectedParts.get("Color"), selectedParts.get("Engine"), selectedParts.get("GearBox"), selectedParts.get("Seats"), selectedParts.get("Airco"), selectedParts.get("Wheels"));
-
-          DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy 'at' H:mm");
-          System.out.println();
-          System.out.println("The estimated completion date for the order is: " + estimatedCompletionDate.format(formatter));
-
-          System.out.println();
-          System.out.println("Press ENTER to continue...");
-          Scanner scanner3 = new Scanner(System.in);
-          scanner3.nextLine();
-
-          run(); //TODO check if dit is de manier juiste redirect naar zichzelf
-        }
-        case -1 -> this.garageHolderActionsOverviewUI.run();
+      if (allAssemblyTasks.isEmpty()) {
+        System.out.println("There are currently no pending tasks for this work post");
+        continue;
       }
-    } while (choice != -1 && (choice != 1));
-  }
 
-  private static void displayCarOrders(List<String> carOrders) {
-    for (int i = 0; i < carOrders.size(); i++) {
-      System.out.println(String.format("%2d", (i + 1)) + ": " + carOrders.get(i));
-    }
-  }
+      Optional<Integer> chosenAssemblyTaskIdOptional = displayChooseAssemblyTask(allAssemblyTasks);
 
-  private static Optional<Integer> displayChooseCarModel(Map<Integer, String> carModels) {
-    Scanner scanner = new Scanner(System.in);
-    int carModelId;
+      if (chosenAssemblyTaskIdOptional.isEmpty()) continue; // TODO: show feedback
 
-    do {
-      System.out.println();
-      System.out.println("Please choose the model:");
-      carModels.forEach((id, name) -> System.out.println(String.format("%2d", id) + ": " + name));
-      System.out.println("-1: Go back");
+      int chosenAssemblyTaskId = chosenAssemblyTaskIdOptional.get();
 
-      carModelId = scanner.nextInt();
+      performAssemblyTasksController.setActiveTask(chosenWorkPostId, chosenAssemblyTaskId);
 
-      if (carModelId == -1) return Optional.empty();
-
-    } while (!carModels.containsKey(carModelId));
-
-    System.out.println("Chosen model: " + carModels.get(carModelId));
-    return Optional.of(carModelId);
-  }
-
-
-  private static Optional<Map<String, String>> displayOrderingForm(Map<String, List<String>> parts) {
-    Map<String, String> selectedParts = new HashMap<>();
-
-    for (Map.Entry<String, List<String>> part : parts.entrySet()) {
-      Scanner scanner = new Scanner(System.in);
-      int optionIndex;
-      List<String> options = part.getValue();
+      List<String> actions = performAssemblyTasksController.giveAssemblyTaskActions(chosenWorkPostId, chosenAssemblyTaskId);
 
       System.out.println();
-      System.out.println("Choose the option for: " + part.getKey());
+      System.out.println("Execute the following actions:");
+      displayActions(actions);
 
-      do {
-        for (int i = 0; i < options.size(); i++) {
-          System.out.println(String.format("%2d", i) + ": " + options.get(i));
-        }
-        System.out.println("-1: Cancel placing the order");
+      int duration = displayInputMinutes();
+      performAssemblyTasksController.completeAssemblyTask(chosenWorkPostId, duration);
 
-        optionIndex = scanner.nextInt();
+      this.carMechanicActionsOverviewUI.run();
 
-        if (optionIndex == -1) {
-          return Optional.empty();
-        }
-      } while (optionIndex < 0 || optionIndex > options.size() - 1);
-
-      selectedParts.put(part.getKey(), part.getValue().get(optionIndex));
-      System.out.println("Chosen: " + options.get(optionIndex));
+      break; // if we reach this point, the use case is done, java call stack will now return to the previous UI
     }
-    return Optional.of(selectedParts);
   }
-}
