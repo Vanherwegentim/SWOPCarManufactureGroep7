343aa58cbae8e6a445cce3d9adb25bef5c9da76b
RobbeRamon <robbe.ramon@gmail.com>
2022-05-22 20:14:12 +0200

    Remove unused methods in the controller factory

--- src/main/java/be/kuleuven/assemassit/Controller/ControllerFactory.java
+++ src/main/java/be/kuleuven/assemassit/Controller/ControllerFactory.java
@@ -6,33 +6,10 @@ import be.kuleuven.assemassit.Domain.GarageHolder;
 import be.kuleuven.assemassit.Domain.ProductionStatistics;
 import be.kuleuven.assemassit.Repositories.GarageHolderRepository;
 
-import java.time.LocalTime;
-
 public class ControllerFactory {
-  private final AssemblyLine assemblyLine;
-  private final CarManufactoringCompany carManufactoringCompany;
-  private GarageHolder loggedInGarageHolder;
-
-  public ControllerFactory() {
-    this.assemblyLine = new AssemblyLine();
-    this.carManufactoringCompany = new CarManufactoringCompany(LocalTime.of(6, 0), LocalTime.of(22, 0), assemblyLine);
-  }
-
-  public String giveLoggedInGarageHolderName() {
-    return loggedInGarageHolder.getName();
-  }
 
   public LoginController createLoginController(ControllerFactoryMiddleWare controllerFactoryMiddleWare) {
     return new LoginController(new GarageHolderRepository(), controllerFactoryMiddleWare);
-  }
-
-  /**
-   * Generate an instance of the order controller
-   *
-   * @return a new instance of the order controller
-   */
-  public OrderNewCarController createOrderNewCarController() {
-    return new OrderNewCarController(carManufactoringCompany, loggedInGarageHolder);
   }
 
   /**
@@ -49,29 +26,11 @@ public class ControllerFactory {
   /**
    * Generate an instance of the checkOrderDetailsController
    *
-   * @return a new instance of the checkOrderDetailsController
-   */
-  public CheckOrderDetailsController createCheckOrderDetailsController() {
-    return new CheckOrderDetailsController(loggedInGarageHolder);
-  }
-
-  /**
-   * Generate an instance of the checkOrderDetailsController
-   *
    * @param loggedInGarageHolder The logged in garage holder
    * @return a new instance of the checkOrderDetailsController
    */
   public CheckOrderDetailsController createCheckOrderDetailsController(GarageHolder loggedInGarageHolder) {
     return new CheckOrderDetailsController(loggedInGarageHolder);
-  }
-
-  /**
-   * Generate an instance of the performAssemblyTasksController
-   *
-   * @return a new instance of the performAssemblyTasksController
-   */
-  public PerformAssemblyTasksController createPerformAssemblyTasksController() {
-    return new PerformAssemblyTasksController(assemblyLine, carManufactoringCompany);
   }
 
   /**
@@ -83,15 +42,6 @@ public class ControllerFactory {
    */
   public PerformAssemblyTasksController createPerformAssemblyTasksController(AssemblyLine assemblyLine, CarManufactoringCompany carManufactoringCompany) {
     return new PerformAssemblyTasksController(assemblyLine, carManufactoringCompany);
-  }
-
-  /**
-   * Generate an instance of the checkAssemblyLineStatusController
-   *
-   * @return a new instance of the checkAssemblyLineStatusController
-   */
-  public CheckAssemblyLineStatusController createCheckAssemblyLineStatusController() {
-    return new CheckAssemblyLineStatusController(assemblyLine);
   }
 
   /**
@@ -117,32 +67,10 @@ public class ControllerFactory {
   /**
    * Generate an instance of the checkProductionStatisticsController
    *
-   * @return a new instance of the checkProductionStatisticsController
-   */
-  public AdaptSchedulingAlgorithmController createAdaptSchedulingAlgorithmController() {
-    return new AdaptSchedulingAlgorithmController(assemblyLine);
-  }
-
-  /**
-   * Generate an instance of the checkProductionStatisticsController
-   *
    * @param assemblyLine The assembly line instance that should be used for creating the controller
    * @return a new instance of the checkProductionStatisticsController
    */
   public AdaptSchedulingAlgorithmController createAdaptSchedulingAlgorithmController(AssemblyLine assemblyLine) {
     return new AdaptSchedulingAlgorithmController(assemblyLine);
-  }
-
-
-  public CarManufactoringCompany getCarManufactoringCompany() {
-    return carManufactoringCompany;
-  }
-
-  public GarageHolder getLoggedInGarageHolder() {
-    return loggedInGarageHolder;
-  }
-
-  public AssemblyLine getAssemblyLine() {
-    return assemblyLine;
   }
 }
